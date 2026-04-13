3d248e9942a7edcadd30b2cf500a3d0d9307787b
Neehru Tumber <neehru25.tumber@gmail.com>
2026-04-13 16:48:38 +0100

    home buttons centered

--- src/camera.jsx
+++ src/camera.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { useEffect } from 'react';
 import App from './App';
 import './App.css'
 import './camera.css'
+import JSMpeg from 'jsmpeg';
 
 function Camera(){
+
+    useEffect(() => {
+        const canvas = document.getElementById('videoCanvas');
+        const url = 'ws://192.168.0.113:9999';
+        new JSMpeg(url, { canvas: canvas, autoplay: true });
+    }, []);
 
     return (
         <>
             <div id="camera_feed">
                 <img src="src/assets/camera_feed_temp.png" alt="video feed temp image"/>
+                {/* <canvas id="videoCanvas" width="640" height="480"></canvas> */}
             </div>
         </>
     )
